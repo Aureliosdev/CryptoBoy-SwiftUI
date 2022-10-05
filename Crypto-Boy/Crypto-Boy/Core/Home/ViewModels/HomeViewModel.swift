6c5c8e99521f0711ee3b2c5b615fe65b6358bbeb
Abylay <aurelioleclarke@gmail.com>
2022-10-05 23:31:39 +0600

    Crypto update 1.1.0
    Added swift file

---
+++ Crypto-Boy/Crypto-Boy/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  Crypto-Boy
+//
+//  Created by Aurelio Le Clarke on 05.10.2022.
+//
+
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+    
+    
+    func fetchData() {
+        
+        
+    }
+}
---
+++ Foreground.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+let x = 14
+let y = 2.5
+let result = Double(x) * y
